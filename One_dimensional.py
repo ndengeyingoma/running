a4e87c0d392189cd1af821628574c1184b51188c
INFINITY INNOVATION GROUP Ltd <96722820+ndengeyingoma@users.noreply.github.com>
2024-10-23 19:40:12 +0300

    Add files via upload
    Signed-off-by: INFINITY INNOVATION GROUP Ltd <96722820+ndengeyingoma@users.noreply.github.com>

---
+++ One_dimensional.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# One-dimensional array
+array_1d = np.array([1, 2, 3])
+print("1D Array:", array_1d)
+
+# Multidimensional array
+array_2d = np.array([[1, 2], [3, 4]])
+print("2D Array:\n", array_2d)
--- arange.py
+++ arange.py
@@ -1,4 +1,7 @@
+# arange is a function in NumPy (numpy.arange()) that generates an array with 
+# evenly spaced values within a specified range.
 
 import numpy as np
+# This creates an array starting at 1, ending before 10, and incrementing by 2.
 arr = np.arange(1, 10, 2)
 print(arr)
